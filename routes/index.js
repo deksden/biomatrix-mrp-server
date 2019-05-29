048594e0fe383ed23939855dc924bed7dc7859a9
deksden <deksden@deksden.com>
2019-05-29 19:00:38 +0700

    - (enh) es2018 with babel

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import Express from 'express'
+
+var router = Express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
