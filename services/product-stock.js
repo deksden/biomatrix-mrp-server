8bac2ada8ec309696800d88e5369feabe7ffb23f
deksden <deksden@deksden.com>
2019-06-10 21:27:04 +0700

    - (enh) product-stock, products: use globals for storage

--- services/product-stock.js
+++ services/product-stock.js
@@ -2,17 +2,29 @@ import _ from 'lodash'
 import fs from 'fs'
 import path from 'path'
 import Moment from 'moment'
-import {defDateFormat} from './def-date-format.js'
+import { defDateFormat } from './def-date-format.js'
+
+let _stock = []
 
 export class Stock {
   constructor (aPath) {
-    this._stock = JSON.parse(fs.readFileSync(path.format({ dir: aPath, base: 'product-stock.json'})))
-    this._stock.map((item) => {
+    if (aPath) {
+      this.loadFromFile(path.format({ dir: aPath, base: 'product-stock.json' }))
+    }
+  }
+
+  loadFromFile (aFile) {
+    _stock = JSON.parse(fs.readFileSync(aFile))
+    _stock.map((item) => {
       item.date = new Moment(item.date, defDateFormat)
     })
   }
 
   filterByProduct (productId) {
-    return _.filter(this._stock, (stock) => stock.product === productId)
+    return _.filter(_stock, (stock) => stock.product === productId)
+  }
+
+  get items () {
+    return _stock
   }
 }
